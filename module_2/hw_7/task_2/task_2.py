40775fc7e606d31e8b785f9b57c0fe28beaff527
Maxim <maxim2k18gh8hg@gmail.com>
2023-08-02 10:27:29 +0300

    upd hw 7, and change task_2

--- module_2/hw_7/task_2/task_2.py
+++ module_2/hw_7/task_2/task_2.py
@@ -1,19 +1,21 @@
 from random import randint as rand
-# import json
+import json
 
 
 def in_range(start, stop, step):
-    _list = list(({i: rand(1, 100)} for i in range(start, stop, step)))
-    yield _list
+    _list = yield list(({str(i): rand(1, 100)} for i in range(start, stop, step)))
 
 
-for i in in_range(1, 21, 1):
-    print(i)
+json_list = in_range(1, 21, 1)
+
+for data in json_list:
+    print(data)
 
 
-# json_file = "file_task_2.json"
-# with open(json_file, "r") as file:
-#     data = json.load(file)
-# data.extend(list(({i: rand(1, 100)} for i in range(1, 21, 1))))
-# with open(json_file, "w") as file:
-#     json.dump(data, file, indent=4)
+    def write_to_json(json_file):
+        json_object = json.dumps(data, indent=4)
+        with open(json_file, "w") as file:
+            file.write(json_object)
+
+
+    write_to_json("file_task_2.json")
