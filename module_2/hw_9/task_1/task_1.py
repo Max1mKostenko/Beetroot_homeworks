a11df4608120c914e3d085e460de450cc080cf2e
Maxim <maxim2k18gh8hg@gmail.com>
2023-08-13 14:46:29 +0300

    commit for brothew

---
+++ module_2/hw_9/task_1/task_1.py
@@ -0,0 +1,16 @@
+class ContextManager:
+    def __init__(self, file_name, mode):
+        self.file_name = file_name
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.file_name, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+
+with ContextManager("task_1.txt", "w") as file:
+    file.write("Hello World!")
---
+++ module_2/hw_9/task_2.py
@@ -0,0 +1,2 @@
+from task_1.task_1 import ContextManager
+import unittest
