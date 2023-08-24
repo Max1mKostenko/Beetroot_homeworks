fd62a41b2503952721da6ce6c9bf32ee99b6b349
Maxim <maxim2k18gh8hg@gmail.com>
2023-08-24 13:48:28 +0300

    upd hw 3

--- module_3/hw_3/client.py
+++ module_3/hw_3/client.py
@@ -1,32 +1,28 @@
 import socket
 
-
-def caesar_decrypt(encrypted_message, key):
-    decrypted_message = ""
-    for char in encrypted_message:
-        if char.isalpha():
-            shift = key % 26
-            if char.islower():
-                decrypted_char = chr((ord(char) - ord('a' - shift)) % 26 + ord('a'))
-            else:
-                decrypted_char = chr((ord(char) - ord('A' - shift)) % 26 + ord('A'))
-            decrypted_message += decrypted_char
-        else:
-            decrypted_message += char
-    return decrypted_message
-
-
 msgFromClient = "Hello UDP Server"
 bytesToSend = str.encode(msgFromClient)
 serverAddressPort = ("127.0.0.1", 20001)
+
 bufferSize = 1024
 
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-encryption_key = int(input("Enter encryption key: "))  # You can also generate this key randomly
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
-msgFromServer, serverAddress = UDPClientSocket.recvfrom(bufferSize)
-decrypted_message = caesar_decrypt(msgFromServer.decode(), encryption_key)
 
-print("Message from Server (Decrypted):", decrypted_message)
+step = 3
+res = ""
+for char in msgFromClient:
+    if char.isalpha():
+        if char.isupper():
+            num_of_ascii_table = 65
+        else:
+            num_of_ascii_table = 97
+        new_char = chr((ord(char) - num_of_ascii_table + step) % 26 + num_of_ascii_table)
+    else:
+        new_char = char
+    res += new_char
+msg = f"Message from Server {res}"
+print(msg)
--- module_3/hw_3/server.py
+++ module_3/hw_3/server.py
@@ -1,45 +1,28 @@
 import socket
 
-
-def caesar_encrypt(massage, key):
-    encrypted_massage = ""
-    for char in massage:
-        if char.isalpha():
-            shift = key % 26
-            if char.islower():
-                encrypted_char = chr((ord(char) - ord('a' + shift)) % 26 + ord('a'))
-            else:
-                encrypted_char = chr((ord(char) - ord('A' + shift)) % 26 + ord('A'))
-            encrypted_massage += encrypted_char
-        else:
-            encrypted_massage += char
-    return encrypted_massage
-
-
 HOST = "127.0.0.1"
 PORT = 20001
 bufferSize = 1024
+
+msgFromServer = "Hello UDP Client"
+bytesToSend = str.encode(msgFromServer)
+
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((HOST, PORT))
 print("UDP server up and listening")
 
+
 while True:
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    massage = bytesAddressPair[0]
+
+    message = bytesAddressPair[0]
     address = bytesAddressPair[1]
 
-    clientMsg = "Massage from Client:{}".format(massage)
+    clientMsg = "Message from Client:{}".format(message)
     clientIP = "Client IP Address:{}".format(address)
 
     print(clientMsg)
     print(clientIP)
 
-    # Get the encryption key from the client
-    encryption_key = int(input("Enter encryption key: "))  # You can also send this key from the client
-
-    # Encrypt the massage using Caesar cipher
-    encrypted_massage = caesar_encrypt(massage.decode(), encryption_key)
-    encrypted_bytes = str.encode(encrypted_massage)
-
-    UDPServerSocket.sendto(encrypted_bytes, address)
+    UDPServerSocket.sendto(bytesToSend, address)
