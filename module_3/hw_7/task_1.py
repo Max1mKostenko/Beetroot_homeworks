ceade62b0450ca3aefb07e7ed95e3f2785e05bdc
Maxim <maxim2k18gh8hg@gmail.com>
2023-09-03 14:24:05 +0300

    some

--- module_3/hw_7/task_1.py
+++ module_3/hw_7/task_1.py
@@ -1,4 +1,4 @@
-# import asyncio
+import asyncio
 
 
 def fibonacci(num):
@@ -15,10 +15,6 @@ def fibonacci(num):
     return fib_curr
 
 
-n = 9
-print(f"Fibonacci({n}) = {fibonacci(n)}")
-
-
 def factorial(var):
     factorial_ = 1
     for i in range(1, var + 1):
@@ -26,19 +22,10 @@ def factorial(var):
     return factorial_
 
 
-number = 5
-print(f"Factorial({number}) = {number}! = {factorial(number)}")
-
-
 def squares(sqr_num):
     return sqr_num ** 2
-
-
-print(squares(5))
 
 
 def cubic(cubic_num):
     return cubic_num ** 3
 
-
-print(cubic(5))
